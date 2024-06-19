2ce3c79189dcf5bcf6d4ab7d92df136aa884336e
Sergey Vinogradov <boogie_byte@proton.me>
2024-06-19 10:51:43 +0000

    Enable gopls

--- init.lua
+++ init.lua
@@ -469,7 +469,6 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -480,6 +479,8 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
+
+        gopls = {},
 
         lua_ls = {
           -- cmd = {...},
