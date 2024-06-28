ee45fb9a9127d829067158adc9ae568d05b71353
Sergey Vinogradov <boogie_byte@proton.me>
2024-06-28 16:17:15 +0000

    Set colorscheme properly

--- init.lua
+++ init.lua
@@ -141,14 +141,6 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- Set colorscheme
-  {
-    'sainnhe/gruvbox-material',
-    config = function()
-      vim.cmd 'colorscheme gruvbox-material'
-    end,
-  },
-
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
@@ -672,21 +664,14 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+  -- Set colorscheme
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+    config = function()
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 
@@ -697,7 +682,9 @@ require('lazy').setup({
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {},
+    opts = {
+      theme = 'gruvbox-material',
+    },
   },
 
   { -- Highlight, edit, and navigate code
