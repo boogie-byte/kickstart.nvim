693a97abdc095e2d020de43a3890617361e13e31
Sergey Vinogradov <boogie_byte@proton.me>
2024-06-28 21:41:11 +0000

    Remove neovim file search keymap

--- init.lua
+++ init.lua
@@ -304,11 +304,6 @@ require('lazy').setup({
           prompt_title = 'Live Grep in Open Files',
         }
       end, { desc = '[S]earch [/] in Open Files' })
-
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
     end,
   },
 
