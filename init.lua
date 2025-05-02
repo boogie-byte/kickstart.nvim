c7521a1d3ca08331b5307f0930ac39bc25623490
Sergey Vinogradov <boogie_byte@proron.me>
2025-05-02 14:40:37 +0200

    Revert "More convenient o/O keymaps"
    This reverts commit 5e6b4bbd988c523aa0ad08cbf2dd06637c9e7761.

--- init.lua
+++ init.lua
@@ -101,10 +101,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- More convenient o/O keymaps
-vim.keymap.set('n', 'o', 'o<Esc>k')
-vim.keymap.set('n', 'O', 'O<Esc>j')
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
