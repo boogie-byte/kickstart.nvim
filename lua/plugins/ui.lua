1fe1d8814dcd4f50685b3976857d9dc38227c005
Sergey Vinogradov <boogie_byte@proron.me>
2025-07-28 16:46:03 +0200

    Add smart-splits plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -34,6 +34,35 @@ return {
     end,
   },
 
+  -- More intuitive split pane management
+  {
+    'mrjones2014/smart-splits.nvim',
+    config = function()
+      local splits = require 'smart-splits'
+
+      -- resizing splits
+      -- these keymaps will also accept a range,
+      -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+      vim.keymap.set('n', '<A-h>', splits.resize_left)
+      vim.keymap.set('n', '<A-j>', splits.resize_down)
+      vim.keymap.set('n', '<A-k>', splits.resize_up)
+      vim.keymap.set('n', '<A-l>', splits.resize_right)
+
+      -- moving between splits
+      vim.keymap.set('n', '<C-h>', splits.move_cursor_left)
+      vim.keymap.set('n', '<C-j>', splits.move_cursor_down)
+      vim.keymap.set('n', '<C-k>', splits.move_cursor_up)
+      vim.keymap.set('n', '<C-l>', splits.move_cursor_right)
+      vim.keymap.set('n', '<C-\\>', splits.move_cursor_previous)
+
+      -- swapping buffers between windows
+      vim.keymap.set('n', '<leader><leader>h', splits.swap_buf_left, { desc = 'Swap windows left' })
+      vim.keymap.set('n', '<leader><leader>j', splits.swap_buf_down, { desc = 'Swap windows down' })
+      vim.keymap.set('n', '<leader><leader>k', splits.swap_buf_up, { desc = 'Swap windows up' })
+      vim.keymap.set('n', '<leader><leader>l', splits.swap_buf_right, { desc = 'Swap windows left' })
+    end,
+  },
+
   -- Pretty statusline
   {
     'nvim-lualine/lualine.nvim',
