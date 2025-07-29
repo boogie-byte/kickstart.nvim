83f5771a2ee7e00d1da3dc21f798cc332d0e3b25
Sergey Vinogradov <boogie_byte@proron.me>
2025-07-29 11:45:39 +0200

    Clean up keymaps

--- init.lua
+++ init.lua
@@ -80,14 +80,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.diagnostic.config { jump = { float = true }, virtual_text = true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
